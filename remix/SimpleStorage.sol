9fb22d3e9233a274e096ca72f06b3e65e37de9a0
amanbthakkar <aman.thakkar@somaiya.edu>
2022-06-08 17:21:57 +0530

    StorageFactory stuff

--- remix/SimpleStorage.sol
+++ remix/SimpleStorage.sol
@@ -29,4 +29,4 @@ contract SimpleStorage{
         people.push(People(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber; 
     }
-}
+};
---
+++ remix/StorageFactory.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorage() public {
+        simpleStorageArray.push(new SimpleStorage());    //store all SimpleStorage deployments
+    }
+
+    function contractCount() view public returns(uint256) {
+        return simpleStorageArray.length;
+    }
+
+    //what if you want to call SimpleStorage's store function from here?
+    //need contract address, and ABI (ABI can be auto-gotten when importing a contract)
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageFavNumber) public {
+        SimpleStorage ss = simpleStorageArray[_simpleStorageIndex];
+        //Because our array is of type SimpleStorage, it comes with ABI inside
+        //if it was just an array of type 'address', we would need to --> SimpleStorage(simpleStorageArray[_simpleStorageIndex]);
+        ss.store(_simpleStorageFavNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) view public returns(uint256) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+}
